a85a9499f5055db0b472f519a29c66fb0f8b2bbe
deb-user <kasper_furnesvik@hotmail.com>
2020-08-06 10:09:56 +0200

    Just added some basic makefile examples

---
+++ proxy_in/104server.c
@@ -0,0 +1,156 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+
+#include "cs104_slave.h"
+
+#include "hal_thread.h"
+#include "hal_time.h"
+
+static bool running = true;
+
+void
+sigint_handler(int signalId)
+{
+    running = false;
+}
+
+void
+printCP56Time2a(CP56Time2a time)
+{
+    printf("%02i:%02i:%02i %02i/%02i/%04i", CP56Time2a_getHour(time),
+                             CP56Time2a_getMinute(time),
+                             CP56Time2a_getSecond(time),
+                             CP56Time2a_getDayOfMonth(time),
+                             CP56Time2a_getMonth(time),
+                             CP56Time2a_getYear(time) + 2000);
+}
+
+/* Callback handler to log sent or received messages (optional) */
+static void
+rawMessageHandler(void* parameter, IMasterConnection conneciton, uint8_t* msg, int msgSize, bool sent)
+{
+    if (sent)
+        printf("SEND: ");
+    else
+        printf("RCVD: ");
+
+    int i;
+    for (i = 0; i < msgSize; i++) {
+        printf("%02x ", msg[i]);
+    }
+
+    printf("\n");
+}
+
+/* Handler to handle incoming ASDUs */
+static bool
+asduHandler(void* parameter, IMasterConnection connection, CS101_ASDU asdu)
+{
+    /* the asdu parameter contains the actual asdu that was received */
+    /* Add code that re-sends the asdu through an udp-socket associated with the diode */
+
+    return false;
+}
+
+int
+main(int argc, char** argv)
+{
+    /* Add Ctrl-C handler */
+    signal(SIGINT, sigint_handler);
+
+    /* create a new slave/server instance with default connection parameters and
+     * default message queue size */
+    CS104_Slave slave = CS104_Slave_create(10, 10);
+
+    CS104_Slave_setLocalAddress(slave, "0.0.0.0");
+
+    /* Set mode to a single redundancy group
+     * NOTE: library has to be compiled with CONFIG_CS104_SUPPORT_SERVER_MODE_SINGLE_REDUNDANCY_GROUP enabled (=1)
+     */
+    CS104_Slave_setServerMode(slave, CS104_MODE_SINGLE_REDUNDANCY_GROUP);
+
+    /* get the connection parameters - we need them to create correct ASDUs -
+     * you can also modify the parameters here when default parameters are not to be used */
+    CS101_AppLayerParameters alParams = CS104_Slave_getAppLayerParameters(slave);
+
+    /* when you have to tweak the APCI parameters (t0-t3, k, w) you can access them here */
+    CS104_APCIParameters apciParams = CS104_Slave_getConnectionParameters(slave);
+
+    printf("APCI parameters:\n");
+    printf("  t0: %i\n", apciParams->t0);
+    printf("  t1: %i\n", apciParams->t1);
+    printf("  t2: %i\n", apciParams->t2);
+    printf("  t3: %i\n", apciParams->t3);
+    printf("  k: %i\n", apciParams->k);
+    printf("  w: %i\n", apciParams->w);
+
+    /* set the callback handler for the clock synchronization command */
+    CS104_Slave_setClockSyncHandler(slave, clockSyncHandler, NULL);
+
+    /* set the callback handler for the interrogation command */
+    CS104_Slave_setInterrogationHandler(slave, interrogationHandler, NULL);
+
+    /* set handler for other message types */
+    CS104_Slave_setASDUHandler(slave, asduHandler, NULL);
+
+    /* set handler to handle connection requests (optional) */
+    CS104_Slave_setConnectionRequestHandler(slave, connectionRequestHandler, NULL);
+
+    /* set handler to track connection events (optional) */
+    CS104_Slave_setConnectionEventHandler(slave, connectionEventHandler, NULL);
+
+    /* uncomment to log messages */
+    CS104_Slave_setRawMessageHandler(slave, rawMessageHandler, NULL);
+
+    CS104_Slave_start(slave);
+
+    if (CS104_Slave_isRunning(slave) == false) {
+        printf("Starting server failed!\n");
+        goto exit_program;
+    }
+
+    int16_t scaledValue = 0;
+
+    while (running) {
+
+        Thread_sleep(1000);
+
+        CS101_ASDU newAsdu = CS101_ASDU_create(alParams, false, CS101_COT_PERIODIC, 0, 1, false, false);
+
+        InformationObject io = (InformationObject) MeasuredValueScaled_create(NULL, 110, scaledValue, IEC60870_QUALITY_GOOD);
+
+        scaledValue++;
+
+        CS101_ASDU_addInformationObject(newAsdu, io);
+
+        InformationObject_destroy(io);
+
+        /* Add ASDU to slave event queue */
+        CS104_Slave_enqueueASDU(slave, newAsdu);
+
+        CS101_ASDU_destroy(newAsdu);
+    }
+
+    CS104_Slave_stop(slave);
+
+exit_program:
+    CS104_Slave_destroy(slave);
+
+    Thread_sleep(500);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
