695d89062acc572f8f199a32ba931bb2daf8d74c
deb-user <kasper_furnesvik@hotmail.com>
2020-08-13 10:41:32 +0200

    Added the rand_send() program

--- udp_test/rand_send.py
+++ udp_test/rand_send.py
@@ -12,17 +12,23 @@ PORT = 60000
 
 def main() :
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    elems = []
-    for e in range(1400) :
-        elems.append(e)
+
+    # elems = []
+    # for e in range(1400) :
+    #     elems.append(e)
 
     print("Sending random numbers...")
     try :
         while True :
-            # n = random.randint(-30, 30)
-            data = bytes(str(elems), 'utf-8')
+            n = []
+            n.append(random.randint(-30, 30))
+            n.append(random.randint(-30, 30))
+            if n[0] < 1 :
+                for i in range(10) :
+                    n.append(random.randint(-30, 30))
+            data = bytes(str(n), 'utf-8')
             sock.sendto(data, (HOST, PORT))
-            # sleep(0.5)
+            sleep(0.5)
     finally :
         sock.close()
 
